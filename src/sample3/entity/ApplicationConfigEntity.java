270a0dfe57ea986a64393e2a2bb3bf565b716680
Jorge Marin <yoryo777@gmail.com>
2025-05-19 15:15:05 -0600

    Create ApplicationConfigEntity.java

---
+++ src/sample3/entity/ApplicationConfigEntity.java
@@ -0,0 +1,21 @@
+package sample3.entity;
+
+public class ApplicationConfigEntity {
+	
+	private String key;
+	private String value;	
+	
+	public String getKey() {
+		return key;
+	}
+	public String getValue() {
+		return value;
+	}
+	public void setKey(String key) {
+		this.key = key;
+	}
+	public void setValue(String value) {
+		this.value = value;
+	}			
+
+}
