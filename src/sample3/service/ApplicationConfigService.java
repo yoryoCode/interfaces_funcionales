2495345be53a971d57702684f99de13d2af216c6
Jorge Marin <yoryo777@gmail.com>
2025-05-19 15:21:12 -0600

    Delete src/sample3/service/ApplicationConfigService.java

--- src/sample3/service/ApplicationConfigService.java
+++
@@ -1,7 +0,0 @@
-package sample3.service;
-
-import sample3.dto.ApplicationConfigDTO;
-
-public interface ApplicationConfigService {
-	public ApplicationConfigDTO getApplicationConfig(String key);
-}
