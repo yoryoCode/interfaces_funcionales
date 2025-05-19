6ebc887dd5fb6b71214086a56a17e15e857fd60f
Jorge Marin <yoryo777@gmail.com>
2025-05-19 15:16:10 -0600

    Create ApplicationConstants.java

---
+++ src/sample3/utils/ApplicationConstants.java
@@ -0,0 +1,8 @@
+package sample3.utils;
+
+public class ApplicationConstants {
+	
+	public static String SUBJECT_WINNER_MAIL = "¡Hola, te has ganado un premio!"; 
+	public static String SUBJECT_FAQ_MAIL = "¡Hola, tenemos noticias para ti!"; 
+	
+}
